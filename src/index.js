f1b290230dda96f58521f4d5bc3babc9380a7652
Chris Tai <christopherctai@gmail.com>
2023-02-16 18:49:38 -0800

    Create modules and test noteFactory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import { noteFactory } from './info-holders.js'
+
+
 console.log("hello world");
+
+const noteOne = noteFactory("hi", "test description", "May 1st", "urgent");
+console.log(noteOne);
---
+++ src/info-holders.js
@@ -0,0 +1,15 @@
+// Information holders 
+// Objects that hold information for the to-do list 
+
+
+const noteFactory = (title, description, dueDate, priority) => {
+    
+    
+    return {
+        title, 
+        description,
+        dueDate, 
+        priority
+    }
+}
+
