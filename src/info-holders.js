58eeba44e7c0c8fd156f308a5473b4339c0b0e2b
Chris Tai <christopherctai@gmail.com>
2023-02-16 18:56:25 -0800

    Fix export issue for noteFactory

--- src/info-holders.js
+++ src/info-holders.js
@@ -1,10 +1,7 @@
 // Information holders 
 // Objects that hold information for the to-do list 
 
-
-const noteFactory = (title, description, dueDate, priority) => {
-    
-    
+export const noteFactory = (title, description, dueDate, priority) => {
     return {
         title, 
         description,
