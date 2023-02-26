dc16e80c66a66c775d2a2078010c09b1de6b6810
Chris Tai <christopherctai@gmail.com>
2023-02-25 21:42:32 -0800

    Check to see if local storage is available

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import * as InfoHolders from './info-holders.js';
-import * as Controllers from './controllers.js';
-import * as Structurers from './structurers.js';
 import * as Displayers from './displayers.js';
-import * as Utils from './utils.js';
+
 
 import { format } from 'date-fns';
 
